a214f499b7a672087610d43bc5825fb0ab94e1d9
admin <havocmadha200@gmail.com>
2026-06-15 12:24:44 +0530

    switch smtp to tls

--- main.py
+++ main.py
@@ -819,10 +819,10 @@ conf = ConnectionConfig(
     MAIL_FROM="havocmadhan200@gmail.com",
 
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_PORT=465,
+    MAIL_PORT=587,
 
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
 
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
