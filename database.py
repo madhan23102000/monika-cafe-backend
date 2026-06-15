5df226c8b6e2539ddfc279910ac2fdc7e1970f26
admin <havocmadha200@gmail.com>
2026-06-15 08:10:17 +0530

    use render database url

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL
+    DATABASE_URL,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
