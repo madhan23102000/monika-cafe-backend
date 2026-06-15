e72fd7962c48a8b9d04843e21848ff42c9c054ac
admin <havocmadha200@gmail.com>
2026-06-15 08:16:01 +0530

    fix database env

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
