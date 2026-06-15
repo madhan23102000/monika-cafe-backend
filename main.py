7820e3fb8d6da69c71a04bc0577e2233866d8158
admin <havocmadha200@gmail.com>
2026-06-15 12:02:23 +0530

    fix gmail smtp

--- main.py
+++ main.py
@@ -816,11 +816,15 @@ conf = ConnectionConfig(
     MAIL_USERNAME="havocmadhan200@gmail.com",
     MAIL_PASSWORD="dyqjnafyjzifnzje",
     MAIL_FROM="havocmadhan200@gmail.com",
-    MAIL_PORT=587,
+
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True
+    MAIL_PORT=465,
+
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
 
 
