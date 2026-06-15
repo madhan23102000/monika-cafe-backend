86d8fcbe243bac2006f7ede19ea366eeaffce65b
admin <havocmadha200@gmail.com>
2026-06-15 13:21:58 +0530

    clean imports

--- main.py
+++ main.py
@@ -816,9 +816,9 @@ def download_invoice(
 # ======================
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_USERNAME=os.getenv("havocmadhan200@gmail.com"),
+    MAIL_PASSWORD=os.getenv("knktpaqxbtlrvizj"),
+    MAIL_FROM=os.getenv("havocmadhan200@gmail.com"),
 
     MAIL_SERVER="smtp.gmail.com",
     MAIL_PORT=587,
