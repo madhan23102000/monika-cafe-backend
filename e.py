ea88a874c81802bb31fe375b12a5996689ae9f9f
admin <havocmadha200@gmail.com>
2026-06-21 13:06:46 +0530

    Recovered monika cafe code

---
+++ e.py
@@ -0,0 +1,161 @@
+[33mcommit 86d8fcbe243bac2006f7ede19ea366eeaffce65b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 13:21:58 2026 +0530
+
+    clean imports
+
+[33mcommit 34ce9564aeb94293b7e179753060db14e2d8f379[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 13:15:03 2026 +0530
+
+    fix mail env
+
+[33mcommit 7ecf99b05cfec800ef906288700efaefd1640b44[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 13:09:19 2026 +0530
+
+    clean imports
+
+[33mcommit cfaa0befefd70f166f791d45ee90f1d18219bfa0[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:51:56 2026 +0530
+
+    fix email env
+
+[33mcommit 2dede774141d8ba76448a60bf9ccf3046fbdc92b[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:45:09 2026 +0530
+
+    move email credentials to env
+
+[33mcommit e310ba55a54cae393ac80f99a9e8d9cd2ca3ac66[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:36:31 2026 +0530
+
+    proper smtp debug
+
+[33mcommit 8b934070bbd56c978e21ce83aa191ee0b9e86465[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:32:06 2026 +0530
+
+    smtp connection test
+
+[33mcommit a214f499b7a672087610d43bc5825fb0ab94e1d9[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:24:44 2026 +0530
+
+    switch smtp to tls
+
+[33mcommit f61a7e197074c7d50b5787a2ed594e2d7d1e1294[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:18:08 2026 +0530
+
+    email error debug
+
+[33mcommit 889568dd703193a814849f62e965d0c466aa9742[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:11:24 2026 +0530
+
+    test email api
+
+[33mcommit 7820e3fb8d6da69c71a04bc0577e2233866d8158[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 12:02:23 2026 +0530
+
+    fix gmail smtp
+
+[33mcommit 23c633885ece5ded23837c5ee5e353059e36dec4[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 11:55:36 2026 +0530
+
+    install fastapi-mail
+
+[33mcommit d12cdec211e1b40e4b95558c14b9c1519b89672c[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 11:52:15 2026 +0530
+
+    add email endpoint
+
+[33mcommit adc4fd2d9a76d34c5582b5711185781b7d9ca875[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 11:46:18 2026 +0530
+
+    email config
+
+[33mcommit e72fd7962c48a8b9d04843e21848ff42c9c054ac[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 08:16:01 2026 +0530
+
+    fix database env
+
+[33mcommit 5df226c8b6e2539ddfc279910ac2fdc7e1970f26[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 08:10:17 2026 +0530
+
+    use render database url
+
+[33mcommit 80f70a21c71341aeae2b858e9e7f416d50b8cdb5[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 08:07:36 2026 +0530
+
+    remove pydantic pin
+
+[33mcommit c03f04ba5b27521390bb5bf0074066f1d5064851[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Mon Jun 15 07:27:30 2026 +0530
+
+    fix requirements
+
+[33mcommit 6f05b159f1c629268b9b2ebb9956b3d097e5813c[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 21:29:45 2026 +0530
+
+    remove pydantic core
+
+[33mcommit 5330e82953d58f085f690fec178fbca37d6f22c3[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 21:13:36 2026 +0530
+
+    fix pydantic version
+
+[33mcommit 1e0afd529b4b4fb416c77ee013f771e9d32243db[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 20:51:28 2026 +0530
+
+    remove email module
+
+[33mcommit 286b1099a791f029aadee8db751defda506f2a7b[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 20:47:06 2026 +0530
+
+    fix pydantic fastapi-mail compatibility
+
+[33mcommit ba846c1b531baf5336b954228d859388b474c3ec[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 20:43:35 2026 +0530
+
+    add missing packages
+
+[33mcommit 97e2ab7138285104e13bc0d5cb45da4aa92b7eee[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 20:36:23 2026 +0530
+
+    add reportlab dependency
+
+[33mcommit 73136c7ce0738726543c4ea6b78e35b39eb41668[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sun Jun 14 20:32:36 2026 +0530
+
+    fix database url
+
+[33mcommit f4c3eddb8e98173c72a073f4feda2fe61b7b7336[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sat Jun 13 20:16:22 2026 +0530
+
+    cleanup git files
+
+[33mcommit 246a7c0365384bc05d0b013d67984cf42a07c688[m
+Author: admin <havocmadha200@gmail.com>
+Date:   Sat Jun 13 20:12:32 2026 +0530
+
+    Cafe backend completed
---
+++ tore --source=86d8fcb main.py
@@ -0,0 +1,27 @@
+[33m86d8fcb[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m clean imports
+[33m34ce956[m fix mail env
+[33m7ecf99b[m clean imports
+[33mcfaa0be[m fix email env
+[33m2dede77[m move email credentials to env
+[33me310ba5[m proper smtp debug
+[33m8b93407[m smtp connection test
+[33ma214f49[m switch smtp to tls
+[33mf61a7e1[m email error debug
+[33m889568d[m test email api
+[33m7820e3f[m fix gmail smtp
+[33m23c6338[m install fastapi-mail
+[33md12cdec[m add email endpoint
+[33madc4fd2[m email config
+[33me72fd79[m fix database env
+[33m5df226c[m use render database url
+[33m80f70a2[m remove pydantic pin
+[33mc03f04b[m fix requirements
+[33m6f05b15[m remove pydantic core
+[33m5330e82[m fix pydantic version
+[33m1e0afd5[m remove email module
+[33m286b109[m fix pydantic fastapi-mail compatibility
+[33mba846c1[m add missing packages
+[33m97e2ab7[m add reportlab dependency
+[33m73136c7[m fix database url
+[33mf4c3edd[m cleanup git files
+[33m246a7c0[m Cafe backend completed
